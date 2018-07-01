cc2f4b750d460d493f45d0650356d3e4a18f1d29
discoshrimp <roydega@gmail.com>
2018-07-01 16:13:01 -0700

    started the code

---
+++ javascript/function.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDolU4ZzPmtfX-bOd9R6Esu7vdCjol36KA",
+    authDomain: "train-schedule-91818.firebaseapp.com",
+    databaseURL: "https://train-schedule-91818.firebaseio.com",
+    projectId: "train-schedule-91818",
+    storageBucket: "",
+    messagingSenderId: "120084131527"
+  };
+  firebase.initializeApp(config);
+
+  var database=firebase.database()
